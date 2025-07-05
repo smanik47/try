8584fa777a7f32dcae29956f04ef4f21edeea18d
Manik Sharma <smanik34@gmail.com>
2025-07-05 23:07:37 +0530

    hi

---
+++ src/main/java/com/example/Library/Models/Books.java
@@ -0,0 +1,14 @@
+package com.example.Library.Models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Books {
+    private String name;
+    private int id;
+
+
+}
